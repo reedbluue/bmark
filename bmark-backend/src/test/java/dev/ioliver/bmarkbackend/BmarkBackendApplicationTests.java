a8a0b99cdf7cb503f4b5e2460e8e288a19ee199d
Igor Oliveira <i.oliveira@email.com>
2024-05-04 20:55:41 -0300

    spring project started and added and db mongo configured

---
+++ bmark-backend/src/test/java/dev/ioliver/bmarkbackend/BmarkBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ioliver.bmarkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmarkBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
